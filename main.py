6fd9aed885ff400124e8f24ac6b376f045dcaf90
rahmandiallo <rahmandiallo3@gmail.com>
2024-05-10 11:10:34 +0100

    Move a rectangle by updating the position property

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from kivy.metrics import dp
 from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.vertex_instructions import Rectangle
+from kivy.graphics.context_instructions import Color
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.gridlayout import GridLayout
@@ -83,5 +86,40 @@ class TheLabApp(App):
 
 class CanvasExample1(Widget):
     pass
+
+class CanvasExample2(Widget):
+    pass
+
+class CanvasExample3(Widget):
+    pass
+
+class CanvasExample4(Widget):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Line(points=(100,100,400,500), width=2)
+            Color(0,1,0)
+            Line(circle= (700,500,150), width=2)
+            Line(rectangle= (700,500,150,100), width=2)
+            self.rect = Rectangle(pos=(700,200), size=(150, 100))
+
+    def on_button_a_click(self):
+        #print("foo")
+        x, y = self.rect.pos
+        w, h = self.rect.size
+        inc = dp(10)
+
+        diff = self.width -(x+w)
+        if diff < inc:
+            inc = diff
+        
+        x += inc
+
+        x += dp(10)
+        self.rect.pos = (x, y)
+
+
+
+    
 
 TheLabApp().run()
